a64ebfd2f70996bcbc1951872dbccd07a98b3ebc
oytundemirbilek <oytun1996@gmail.com>
2019-10-25 04:41:14 +0300

    login and signup

--- db_ops.py
+++ db_ops.py
@@ -1,6 +1,5 @@
 import pymysql
 import json
-import pandas as pd
 
 def connect_db():
     try:
@@ -15,30 +14,53 @@ def connect_db():
     except:
         print("Connection Failed")
         return False
-def create_table():
+
+def create_login_table():
     connection = connect_db()
     with connection.cursor() as cursor:
         sql = "CREATE TABLE LoginInfo(\
             LoginId INT AUTO_INCREMENT,\
             Email VARCHAR(100),\
             AccountPassword VARCHAR(100),\
-            PRIMARY KEY (member_id));"
+            PRIMARY KEY (LoginId));"
         cursor.execute(sql)
 
-def signup(username, password):
+def signup(email, password):
     connection = connect_db()
     with connection.cursor() as cursor:
         # Read a single record
         sql = "SELECT LoginId, AccountPassword FROM LoginInfo WHERE Email=%s"
-        cursor.execute(sql, (username,))
+        cursor.execute(sql, (email,))
         result = cursor.fetchone()
-        print(result)
-        
+    if result == None:
+        with connection.cursor() as cursor:
+            # Create a new record
+            sql = "INSERT INTO Customers (Email, AccountPassword) VALUES (%s, %s)"
+            cursor.execute(sql, (email, password))
+        connection.commit()
+        print("You have successfully signed up.")
+        return True
+    else:
+        print("This email address belongs to another account.")
+        return False
+
+def login(email, password):
+    connection = connect_db()
     with connection.cursor() as cursor:
-        # Create a new record
-        sql = "INSERT INTO Customers (Email, AccountPassword) VALUES (%s, %s)"
-        cursor.execute(sql, (username, password))
-    connection.commit()
-    return True
+        # Read a single record
+        sql = "SELECT AccountPassword FROM LoginInfo WHERE Email=%s"
+        cursor.execute(sql, (email,))
+        result = cursor.fetchone()
+    if result == None:
+        print("Email address not found.")
+        return False
+    else:
+        if result == password:
+            print("Login successful.")
+            return True
+        else:
+            print("Wrong password.")
+            return False
+
 #signup -> GET: show form, POST: if user exist print FAIL, else: save -> then print OK signed up
 #login -> success / fail

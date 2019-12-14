1bad0cde974d524753c6bbb7897f6ff4209bfacd
oytundemirbilek <oytun1996@gmail.com>
2019-12-14 21:27:09 +0000

    product tables, add product, timestamp added

--- db_ops.py
+++ db_ops.py
@@ -1,28 +1,80 @@
-import psycopg2
+#import psycopg2 if db is postgres
+import pymysql # if db is mysql
 import json
+from datetime import datetime
+
 class database:
     def __init__(self):
         self.connection = None
-
     def connect_db(self):
         try:
-            self.connection = psycopg2.connect("host='localhost' port='5432' dbname='test00' user='oytun' password='oytun'")
+            #self.connection = psycopg2.connect("host='remotemysql.com' dbname='Y2twfztOBJ' user='Y2twfztOBJ' password='sDi9LakvoN'")
+            self.connection = pymysql.connect(host='remotemysql.com',
+                    user='Y2twfztOBJ',
+                    password='sDi9LakvoN',
+                    db='Y2twfztOBJ')
             print("Connected!")
             return self.connection
         except:
             print("Connection Failed")
             return False
+    
     def create_user_table(self):
         try:
             sql = "CREATE TABLE UserInfo(\
                 UserId SERIAL PRIMARY KEY,\
-                Email VARCHAR NOT NULL,\
-                AccountPassword VARCHAR NOT NULL );"
+                Email VARCHAR(50) NOT NULL,\
+                CreatedOn TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,\
+                AccountPassword VARCHAR(25) NOT NULL );"
             self.connection.cursor().execute(sql,)
             self.connection.commit()
-            print("Table created.")
+            print("User table created.")
         except:
-            print("Table exists.")
+            print("User table exists.")
+    def create_store_table(self):
+        try:
+            sql = "CREATE TABLE StoreInfo(\
+                StoreId SERIAL PRIMARY KEY,\
+                StoreName VARCHAR(50) NOT NULL,\
+                StoreAddress VARCHAR(100) NOT NULL,\
+                CreatedOn TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,\
+                UserId INT REFERENCES UserInfo(UserId));"
+            self.connection.cursor().execute(sql, )
+            self.connection.commit()
+            print("Store table created.")
+        except:
+            print("Store table exists.")
+    def create_product_table(self):
+        try:
+            sql = "CREATE TABLE ProductInfo(\
+                ProductId SERIAL PRIMARY KEY,\
+                ProductName VARCHAR(50) NOT NULL,\
+                ProductCategory VARCHAR(50) NOT NULL,\
+                ProductPrice FLOAT NOT NULL,\
+                ProductDiscount FLOAT NOT NULL,\
+                CreatedOn TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,\
+                StoreId INT REFERENCES StoreInfo(StoreId));"
+            self.connection.cursor().execute(sql,)
+            self.connection.commit()
+            print("Product table created")
+        except:
+            print("Product table exists.")
+    def create_variant_table(self):
+        try:
+            sql = "CREATE TABLE ProductVariantInfo(\
+                ProductVariantId SERIAL PRIMARY KEY,\
+                Color VARCHAR(15),\
+                Size CHAR,\
+                Material VARCHAR(25),\
+                Stock INT NOT NULL,\
+                CreatedOn TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,\
+                ProductId INT REFERENCES ProductInfo(ProductId));"
+            self.connection.cursor().execute(sql, )
+            self.connection.commit()
+            print("Variant table created.")
+        except:
+            print("Variant table exists.")
+
     def signup(self, email, password):      
         with self.connection.cursor() as cursor:
             # Read a single record
@@ -30,7 +82,7 @@ class database:
             cursor.execute(sql, (email,))
             result = cursor.fetchone()
         if result == None:
-            with connection.cursor() as cursor:
+            with self.connection.cursor() as cursor:
                 # Create a new record
                 sql = "INSERT INTO UserInfo (Email, AccountPassword) VALUES (%s, %s)"
                 cursor.execute(sql, (email, password))
@@ -57,18 +109,6 @@ class database:
             else:
                 print("Wrong password.")
                 return False
-    def create_store_table(self):
-        try:
-            sql = "CREATE TABLE StoreInfo(\
-                StoreId SERIAL PRIMARY KEY,\
-                StoreName VARCHAR NOT NULL,\
-                StoreAddress VARCHAR NOT NULL,\
-                UserId INT REFERENCES UserInfo(UserId));"
-            self.connection.cursor().execute(sql, )
-            self.connection.commit()
-            print("Table created.")
-        except:
-            print("Table exists.")
     def new_store(self, name, address, email):
         with self.connection.cursor() as cursor:
             # Read a single record
@@ -87,9 +127,37 @@ class database:
         else:
             print("This store name exists. Please pick another name.")
             return False
+    def add_product(self, name, category, price, discount, storename):
+        with self.connection.cursor() as cursor:
+            # Read a single record
+            sql = "SELECT ProductName FROM ProductInfo WHERE ProductName=%s"
+            cursor.execute(sql, (name,))
+            result = cursor.fetchone()
+        if result == None:
+            with self.connection.cursor() as cursor:
+                # Create a new record
+                sql = "INSERT INTO ProductInfo (ProductName, ProductCategory, ProductPrice, ProductDiscount, StoreId) \
+                    VALUES (%s, %s, %s, %s, (SELECT StoreId from StoreInfo WHERE StoreName = %s))"
+                cursor.execute(sql, (name, category, price, discount, storename))
+            self.connection.commit()
+            print("You have successfully created a product.")
+            return True
+        else:
+            print("This product name exists. Please pick another name.")
+            return False
 
 #db = database()
 #db.connect_db()
-#db.new_store("Test Store 3", "İTÜ Taşkışla", "sample@sample.com")
+#db.create_user_table()
+#db.create_store_table()
+#db.signup('sample@sample.com', 'testsecret')
+#db.login('sample@sample.com', 'testsecret')
+
+#db.new_store("Test Store 2", "İTÜ Gümüşsuyu", "sample@sample.com")
+#db.add_product("sampletrend", "test", 15.5, 0, "Test Store 2")
+#db.create_product_table()
+#db.create_variant_table()
+
+
 
 #All functions are tested.
